6b4e7183d7523b368e838acfd58c0d705ed1e85c
Stanislav Talover <stantal2017@gmail.com>
2021-10-02 02:22:42 +0300

    Homework_1

---
+++ FL17_HW3/homework/js/task1.js
@@ -0,0 +1,83 @@
+class DepositProfit {
+    constructor() {
+        this.initialMoneyAmount = 0;
+        this.minimumAmount = 1000;
+        this.numberOfYears = 1;
+        this.minimumYears = 1;
+        this.percentage = 0.1;
+        this.minPercentage = 0.1;
+        this.maxPercentage = 100;
+        this.earnedPerOneYear = 0;
+        this.totalEarnedPerAllYears = 0;
+        this.totalProfitAndAmount = 0;
+
+        this.startProgram();
+    }
+
+    startProgram() {
+        try {
+            this.inputInitialMoneyAmount();
+            this.inputNumberOfYears();
+            this.inputPercentage();
+            this.calcTotalProfitAndAmount();
+        } catch (error) {
+            this.showInvalidInputMessage(error);
+            depositProfit = new DepositProfit();
+        }
+    }
+
+    inputInitialMoneyAmount() {
+        this.initialMoneyAmount = Number(prompt('Input initial money amount: '));
+        this.totalProfitAndAmount = this.initialMoneyAmount;
+
+        if (Number.isNaN(this.initialMoneyAmount)) {
+            throw 'initialMoneyAmount is NaN';
+        } else if (this.initialMoneyAmount < this.minimumAmount) {
+            throw 'initialMoneyAmount < ' + this.minimumAmount;
+        }
+    }
+
+    inputNumberOfYears() {
+        this.numberOfYears = Number(prompt('Input number of years: '));
+
+        if (Number.isNaN(this.numberOfYears)) {
+            throw 'numberOfYears is NaN';
+        } else if (this.numberOfYears < this.minimumYears) {
+            throw 'numberOfYears < ' + this.minimumYears;
+        }
+    }
+
+    inputPercentage() {
+        this.percentage = Number(prompt('Input percentage: '));
+
+        if (Number.isNaN(this.percentage)) {
+            throw 'percentage is NaN';
+        } else if (this.percentage < this.minPercentage || this.percentage > this.maxPercentage) {
+            throw 'Allowed percentage: ' + this.minPercentage + ' - ' + this.maxPercentage;
+        }
+    }
+
+    showInvalidInputMessage(message) {
+        alert('Invalid input data: \n' + message);
+    }
+
+    calcTotalProfitAndAmount() {
+        for (let i = 0; i < this.numberOfYears; ++i) {
+            this.earnedPerOneYear = this.totalProfitAndAmount * (this.percentage / 100);
+            this.totalEarnedPerAllYears += this.earnedPerOneYear;
+            this.totalProfitAndAmount = this.initialMoneyAmount + this.totalEarnedPerAllYears;
+        }
+
+        this.showTotalProfitAndAmount();
+    }
+
+    showTotalProfitAndAmount() {
+        alert('Initial amount: ' + this.initialMoneyAmount +
+        '\nNumber of years: ' + this.numberOfYears +
+        '\nPercentage of year: ' + this.percentage +
+        '\n\nTotal profit: ' + this.totalEarnedPerAllYears.toFixed(2) +
+        '\nTotal amount: ' + this.totalProfitAndAmount.toFixed(2));
+    }
+}
+
+var depositProfit = new DepositProfit();
---
+++ FL17_HW3/homework/js/task2.js
@@ -0,0 +1,118 @@
+class CasinoRoulette {
+    constructor(skipPrompt = false) {
+        this.currentUserAttempt = 0;
+        this.maxUserAttempts = 3;
+        this.maxPocketNumber = 8;
+        this.randomisedPocket = 0;
+        this.userSelectedPocket = 0;
+        this.attemptPrizes = [100, 50, 25]; // 1st, 2nd, 3rd
+        this.totalPrize = 0;
+
+        this.runGame = skipPrompt || this.getUserConfirmation('Do you want to play a game?');
+        if (this.runGame) {
+            this.startGame();
+        } else {
+            this.showOnscreenMessage('You did not become a billionaire, but can.');
+        }
+    }
+
+    startGame() {
+        while (this.runGame && this.currentUserAttempt < this.maxUserAttempts) {
+            this.randomisedPocket = Math.floor(Math.random() * this.maxPocketNumber);
+            this.askUserForPocket();
+            this.checkUserGuess();
+            
+            ++this.currentUserAttempt;
+        }
+    }
+
+    askUserForPocket() {
+        let gameInfo = 'Allowed pocket number range: ' + 0 + ' - ' + this.maxPocketNumber +
+        '\nAttempts left: ' + (this.maxUserAttempts - this.currentUserAttempt) +
+        '\nTotal prize: ' + this.totalPrize + '$' +
+        '\nPossible prize on current attempt: ' + this.attemptPrizes[this.currentUserAttempt] + '$' +
+        '\n\nInput pocket number: ';
+
+        this.userSelectedPocket = Number(prompt(gameInfo, '0'));
+
+        if (Number.isNaN(this.userSelectedPocket)) {
+            let message = 'UserSelectedPocket is NaN';
+            this.showOnscreenMessage(message);
+            this.askUserForPocket();
+        } else if (this.userSelectedPocket < 0 || this.userSelectedPocket > this.maxPocketNumber) {
+            let message = 'Allowed pocket number range: ' + 0 + ' - ' + this.maxPocketNumber;
+            this.showOnscreenMessage(message);
+            this.askUserForPocket();
+        }
+    }
+
+    checkUserGuess() {
+        if (this.userSelectedPocket === this.randomisedPocket) {
+            let message = 'Congratulation, you won!' +
+            '\nYour prize is: ' + this.attemptPrizes[this.currentUserAttempt] + '$' +
+            '\nDo you want to continue?';
+
+            this.increaseTotalPrize();
+            this.increaseMaxPocketNumber();
+            this.doublePrizesValues();
+            this.resetUserAttempts();
+
+            let continueGame = this.getUserConfirmation(message);
+            if (continueGame) {
+                // continue execution flow normally
+            } else { // user does not want to continue
+                this.askIfUserWantsToPlayAgain();
+            }
+            
+        } else { // user did not guess
+            this.askIfUserWantsToPlayAgain();
+        }
+    }
+
+    askIfUserWantsToPlayAgain() {
+        let message = 'Thank you for your participation.' +
+            '\nYour prize is: ' + this.totalPrize + '$' +
+            '\nDo you want to play again?';
+
+            let playAgain = this.getUserConfirmation(message);
+            if (playAgain) {
+                if (this.maxUserAttempts - this.currentUserAttempt === 1) {
+                    this.runGame = false; // quit game on no attempts left
+                }
+            } else {
+                this.runGame = false;
+            }
+    }
+
+    resetGameOnNoAttemptsLeft() {
+        casinoRoulette = new CasinoRoulette(true);
+    }
+
+    resetUserAttempts() {
+        this.currentUserAttempt = -1;
+    }
+
+    increaseTotalPrize() {
+        this.totalPrize += this.attemptPrizes[this.currentUserAttempt];
+    }
+
+    increaseMaxPocketNumber() {
+        this.maxPocketNumber += 4;
+    }
+
+    doublePrizesValues() {
+        for (let i = 0; i < this.attemptPrizes.length; ++i) {
+            this.attemptPrizes[i] = this.attemptPrizes[i] * 2;
+        }
+    }
+
+    showOnscreenMessage(message) {
+        alert(message);
+    }
+
+    getUserConfirmation(message) {
+        return confirm(message);
+    }
+}
+
+var casinoRoulette = new CasinoRoulette();
